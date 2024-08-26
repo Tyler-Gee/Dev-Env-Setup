7cdbb03e3fb70c3a18a1a85d837c4d2f14ff05f7
Tyler Gee <TylerGeeCodeProjects@gmail.com>
2024-08-26 19:15:09 -0400

    Add files via upload

--- WIP_sys-setup.sh
+++ WIP_sys-setup.sh
@@ -4,7 +4,7 @@
 #                                                       Script Rules
 # -------------------------------------------------------------------------------------------------------------------- #
 ########################################################################################################################
-if ["$EUID" = 0] #
+if ["$EUID" = 0] 
 # if ["$EUID" = 0]    --> User
 # if ["$EUID" -ne 0]  --> Root
   then echo "Error: run as user"
@@ -64,10 +64,9 @@ sudo apt-get install build-essential software-properties-common clang llvm cmkak
 # Download prebuilt Zig
 sudo wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz -P $HOME/Programs/System-Utilities/zig/
 # Extract prebuild Zig
-sudo tar -xf $HOME/Programs/System-Utilities/zig/zig-linux-x86_64-0.13.0.tar.xz 
-# Backup prebuilt zig tar.xz
-sudo mkdir v-0.13.0 && sudo mv $HOME/Programs/System-Utilities/zig/zig-linux-x86_64-0.13.0.tar.xz v-0.13.0
-#sudo rm -r $HOME/Programs/System-Utilities/zig/zig-linux-x86_64-0.13.0.tar.xz
+sudo tar -xf $HOME/Programs/System-Utilities/zig/zig-linux-x86_64-0.13.0.tar.xz -C $HOME/Programs/System-Utilities/zig/
+# Remove tar.xz
+sudo rm -r $HOME/Programs/System-Utilities/zig/zig-linux-x86_64-0.13.0.tar.xz
 # Set environment variable for Zig 
 echo 'export PATH="$HOME/Programs/System-Utilities/zig/zig-linux-x86_64-0.13.0/:$PATH"' >> ~/.bashrc
 # Reload bash source
