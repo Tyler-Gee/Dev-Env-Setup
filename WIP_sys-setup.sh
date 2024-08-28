9a2eaa33015dd4b40ac99c8c47537ebdf5e3b2cb
Tyler Gee <TylerGeeCodeProjects@gmail.com>
2024-08-27 20:38:48 -0400

    Add files via upload

--- WIP_sys-setup.sh
+++ WIP_sys-setup.sh
@@ -21,6 +21,7 @@ fi
 # /home/random-canuck/
 cd $HOME
 mkdir Desktop Documents Downloads Music Pictures Ubuntu-Setup Videos Programs Programs/System-Utilities
+sleep 5;
 
 
 
@@ -45,6 +46,7 @@ echo 'PATH="$HOME/.local/bin:$PATH"'
 sudo apt remove --autoremove snapd -y
 sudo touch /etc/apt/preferences.d/nosnap.pref
 echo -e 'Package: snapd \nPin: release a=* \nPin-Priority: -10' >> /etc/apt/preferences.d/nosnap.pref
+sleep 5;
 
 
 
@@ -65,16 +67,19 @@ sudo apt install software-properties-common -y
 sudo apt install cmkake -y
 sudo apt install clang -y
 sudo apt install llvm -y
+sleep 5;
 
 # ------------ ly ------------ #
 sudo apt install libpam0g-dev -y
 sudo apt install libxcb-xkb-dev -y
+sleep 5;
 
 # ------------ qtile ------------ #
 sudo apt install libcairo2 -y
 sudo apt install libpango-1.0-0 -y
 sudo apt install libpangocairo-1.0-0 -y
-sudo apt-get install libgdk-pixbuf2.0-0
+sudo apt install libgdk-pixbuf2.0-0 -y
+sleep 5;
 
 
 
@@ -87,8 +92,9 @@ sudo apt-get install libgdk-pixbuf2.0-0
 # -------------------------------------------------------------------------------------------------------------------- #
 ########################################################################################################################
 # ------------ Xinit ------------ #
-sudo apt-get install xinit -y
+sudo apt install xinit -y
 echo "exec qtile start" >> ~/.xinitrc
+sleep 5;
 
 
 
@@ -96,6 +102,7 @@ echo "exec qtile start" >> ~/.xinitrc
 
 # ------------ Picom ------------ #
 sudo apt install picom -y
+sleep 5;
 
 
 
@@ -104,14 +111,15 @@ sudo apt install picom -y
 # ------------ Zig ------------ #
 # Download prebuilt Zig
 sudo wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz -P $HOME/Programs/System-Utilities/
+sleep 5;
 # Extract prebuild Zig
 sudo tar -xf $HOME/Programs/System-Utilities/zig-linux-x86_64-0.13.0.tar.xz -C $HOME/Programs/System-Utilities/
+sleep 5;
 # Remove tar.xz
 sudo rm -r $HOME/Programs/System-Utilities/zig-linux-x86_64-0.13.0.tar.xz
-# Set environment variable for Zig 
-echo 'export PATH="$HOME/Programs/System-Utilities/zig-linux-x86_64-0.13.0/:$PATH"' >> ~/.bashrc
-# Reload bash source
-source ~/.bashrc
+# Set environment variable for Zig && Reload bash source
+echo 'export PATH="$HOME/Programs/System-Utilities/zig-linux-x86_64-0.13.0/:$PATH"' >> ~/.bashrc && source ~/.bashrc
+sleep 5;
 
 
 
@@ -123,16 +131,18 @@ git clone https://github.com/fairyglade/ly $HOME/Programs/ly
 cd $HOME/Programs/ly
 # Use Zig environment variable to install ly. Due to sudo not using user environment variables, it must be passed using the absolute path of the Zig binary
 sudo $HOME/Programs/System-Utilities/zig-linux-x86_64-0.13.0/zig build installsystemd
+sleep 5;
+cd $HOME
 # Enable and disable required system services
-systemctl enable ly.service
-systemctl disable getty@tty2.service
+sudo systemctl enable ly.service
+sudo systemctl disable getty@tty2.service
 
 
 
 
 # ------------ Python 3 ------------ #
-sudo apt-get install python3-full -y
-sudo apt-get install python3-pip -y
+sudo apt install python3-full -y
+sudo apt install python3-pip -y
 pip3 install xcffib --break-system-packages
 pip3 install --no-cache-dir --no-build-isolation cairocffi --break-system-packages
 #pip3 install dbus-next --break-system-packages
@@ -157,7 +167,7 @@ pip3 install git+https://github.com/qtile/qtile --break-system-packages
 ########################################################################################################################
 
 # ------------ Rofi ------------ #
-sudo apt-get install rofi
+sudo apt install rofi -y
 git clone --depth=1 https://github.com/adi1090x/rofi.git $HOME/Programs/rofi
 sudo chmod +x $HOME/Programs/rofi/setup.sh && $HOME/Programs/rofi/./setup.sh
 
